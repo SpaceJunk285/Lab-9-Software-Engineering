e6b946bc6529eb32c5443a98643cf3f1d2250b4c
Philip Valvo <p.valvo.schnotalla@gmail.com>
2024-07-25 17:09:23 -0400

    Added menu options

--- pvs_Lab9.py
+++ pvs_Lab9.py
@@ -4,5 +4,27 @@ def encode(password):
         encoded += str(int(i) + 3)
     return encoded
 
+
 if __name__ == '__main__':
-    print(encode(1112))
+    while True:
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit\n")
+
+        option = int(input("Please enter an option:"))
+        password = ""
+        encoded = ""
+
+        if option == 1:
+            password = input("Please enter the password to encode:")
+            encoded = encode(password)
+            print("Your password has been encoded and stored!")
+
+        elif option == 2:
+            original_password = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {password}.")
+
+        elif option == 3:
+            break
